05718a506a84c8f835489b90abe9a7f065d8d47f
Dhananjay <dkgurav0101@gmail.com>
2023-04-12 22:57:52 +0530

    exception file created and tested...

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from  flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
+import sys 
+
 
 #create flask app instance
 app = Flask(__name__)
 
 @app.route("/",methods = ["GET","POST"])
 def index():
-
-     return "Starting Machine learning Project"
+     try:
+          raise Exception("we are testing custom exception")
+     except Exception as e :
+          housing =HousingException(e,sys)
+          logging.info(housing.error_message)
+          logging.info("We are testing logging Module...")
+     
+     return "CI CD pipeline has been established..."
 
 if __name__== '__main__':
      app.run(debug=True)

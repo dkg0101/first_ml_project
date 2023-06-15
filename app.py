82a1ef610bd49c7f1b78a3d14610571d3edffed5
Dhananjay <dkgurav0101@gmail.com>
2023-06-15 10:39:26 +0530

    data validation compleated and all files are updated

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
-from  flask import Flask
+from flask import Flask
+import sys
 from housing.logger import logging
 from housing.exception import HousingException
-import sys 
+app=Flask(__name__)
 
 
-#create flask app instance
-app = Flask(__name__)
-
-@app.route("/",methods = ["GET","POST"])
+@app.route("/",methods=['GET','POST'])
 def index():
-     try:
-          raise Exception("we are testing custom exception")
-     except Exception as e :
-          housing =HousingException(e,sys)
-          logging.info(housing.error_message)
-          logging.info("We are testing logging Module...")
-     
-     return "CI CD pipeline has been established..."
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
+    return "CI CD pipeline has been established."
 
-if __name__== '__main__':
-     app.run(debug=True)
+
+if __name__=="__main__":
+    app.run()
--- housing/pipeline/pipeline.py
+++ housing/pipeline/pipeline.py
@@ -1,18 +1,16 @@
-from housing.config import configuration
-from  housing.config.configuration import Configuration
+from housing.config.configuration import Configuartion
 from housing.logger import logging
 from housing.exception import HousingException
 
-from housing.pipeline import DataIngestionArtifact
+from housing.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 from housing.entity.config_entity import DataIngestionConfig
 from housing.component.data_ingestion import DataIngestion
-
-
-import os, sys
+from housing.component.data_validation import DataValidation
+import os,sys
 
 class Pipeline:
 
-    def __init__(self,config: Configuration = Configuration()) -> None:
+    def __init__(self,config: Configuartion = Configuartion()) -> None:
         try:
             self.config=config
 
@@ -24,33 +22,40 @@ class Pipeline:
             data_ingestion = DataIngestion(data_ingestion_config=self.config.get_data_ingestion_config())
             return data_ingestion.initiate_data_ingestion()
         except Exception as e:
-            raise HousingException(e,sys) from e
-        
+            raise HousingException(e,sys) from e    
 
-    def start_data_validation(self):
-        pass
+
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact) \
+        -> DataValidationArtifact :
+        try:
+            data_validation =  DataValidation(data_validation_config=self.config.get_data_validation_config(),
+                                              data_ingestion_artifact=data_ingestion_artifact
+            )
+            return data_validation.initiate_data_validation()
+        except Exception as e:
+            raise HousingException(e,sys) from e
 
     def start_data_transformation(self):
         pass
 
     def start_model_trainer(self):
-         pass
-    
+        pass
+
     def start_model_evaluation(self):
-         pass
-    
+        pass
+
     def start_model_pusher(self):
         pass
 
-
     def run_pipeline(self):
         try:
-           #data ingestion 
-           data_ingestion_artifact = self.start_data_ingestion()
+            #data ingestion
+
+            data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+
+            
+
+
         except Exception as e:
-              raise HousingException(e,sys) from e 
-        
-              
-              
-               
-       
+            raise HousingException(e,sys) from e
